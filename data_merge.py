84c10bec0c6e773830c0105d483d22b6b7068f0c
Daniel <ddra16@student.aau.dk>
2020-09-29 18:15:54 +0200

    added loop

--- data_merge.py
+++ data_merge.py
@@ -2,28 +2,24 @@ import pandas as pd
 import imdb
 
 moviesDB = imdb.IMDb()
-print(dir(moviesDB))
-
 data = pd.read_csv('Data/movies.csv')
-print(data)
-
-id = data['movieId'][2]
-print(id)
-movie = moviesDB.get_movie(id)
-print(movie.data)
 
 
-title = movie['title']
-year = movie['year']
-rating = movie['rating']
-director = movie['directors']
-country = movie['countries']
-genres = movie['genres']
-votes = movie['votes']
-kind = movie['kind']
-plot = movie['plot']
-#casting = movie['cast']
-
-print(title)
-print(rating)
-print(genres)
+def get_data():
+    for x in range(0,len(data)):
+        id = data['movieId'][x]
+        movie = moviesDB.get_movie(id)
+        title = movie['title']
+        year = movie['year']
+        rating = movie['rating']
+        director = movie['directors']
+        country = movie['countries']
+        genres = movie['genres']
+        votes = movie['votes']
+        kind = movie['kind']
+        plot = movie['plot']
+        aka = movie['akas']
+        #casting = movie['cast']
+        print(movie)
+    
+get_data()
