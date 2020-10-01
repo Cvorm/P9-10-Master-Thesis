0bbc709b4d705a666e349c4281ab067c41ada05c
Daniel <ddra16@student.aau.dk>
2020-10-01 15:52:19 +0200

    added functionality

--- data_merge.py
+++ data_merge.py
@@ -3,10 +3,13 @@ import imdb
 
 moviesDB = imdb.IMDb()
 data = pd.read_csv('Data/movies.csv')
-
-
+def set_header():
+    f = open('Data/csvfile.csv', 'w') #overwrites the file, leaving only a header
+    f.write('title, year, rating\n')
+    f.close()
 def get_data():
-    for x in range(0,len(data)):
+    set_header()
+    for x in range(0,2): #len(data) iteratates through each movie
         id = data['movieId'][x]
         movie = moviesDB.get_movie(id)
         title = movie['title']
@@ -20,6 +23,11 @@ def get_data():
         plot = movie['plot']
         aka = movie['akas']
         #casting = movie['cast']
+        text = str(movie) + ',' + str(year)
         print(movie)
-    
+        f = open('Data/csvfile.csv', 'a')
+        f.write(title + ',' + str(year) + ',' + str(rating) + '\n') #appends chosen features to file
+        f.close()
+
+
 get_data()
