8234448b935f8e9d6bcf7f426bc6a786e0a74bb7
Casper <cvorm16@student.aau.dk>
2021-03-18 15:33:40 +0100

    translated LCE code (most of it)

--- engine/LCE_code/construct_A.py
+++ engine/LCE_code/construct_A.py
@@ -4,6 +4,7 @@ import math
 import pandas as pd
 import scipy as sp
 import scipy.sparse
+from sklearn.neighbors import NearestNeighbors
 
 def sparse(i, j, v, m, n):
     """
@@ -23,27 +24,13 @@ def sparse(i, j, v, m, n):
     """
     return scipy.sparse.csr_matrix((v, (i, j)), shape=(m, n))
 
-def construct_A(x, k, binary):
-    n = x.shape[0]
+def construct_A(X, k, binary=False):
 
-    # x = L2_norm_row(x)
-    s = x * x
-
-    vals, inds =  np.argsort(-s, kind='quicksort')
-    # vals = vals(:, 2:(k+1))
-    vals = vals[r_[:, 2:(k+1)]]
-    inds = inds[r_[:, 2:(k+1)]]
-
-    r = [np.tile(r_[1:n], (vals[:], inds[:]))]
-    a = sparse(r[:, 1], r[:, 2], r[:, 3], n, n)
-    a = np.maximum(a, a.conj().T)
+    nbrs = NearestNeighbors(n_neighbors=1 + k).fit(X)
     if binary:
-        for x in a:
-            if x > 0:
-                x = 1
-        return a
-
-    return a
+        return nbrs.kneighbors_graph(X)
+    else:
+        return nbrs.kneighbors_graph(X, mode='distance')
         # no_duplicates = [list(v) for v in dict(movies).items()]
         # a = [x for x in a]
         # [] = 1
