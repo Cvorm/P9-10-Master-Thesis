29f011204aaac8d48a435840b3a3355885d5b867
Casper <cvorm16@student.aau.dk>
2021-03-19 14:41:41 +0100

    translated LCE code (most of it)

--- engine/LCE_code/DEMO.py
+++ engine/LCE_code/DEMO.py
@@ -7,26 +7,34 @@ import scipy.sparse
 import scipy.io
 from construct_A import *
 from LCE_Beta0 import *
+import sklearn as sk
 # import tfidf
 import h5py
 from NDCG import *
 eps = 7. / 3 - 4. / 3 - 1
 
 def L2_norm_row(X):
-    return sp.sparse.spdiags(1. / (np.sqrt(sum(X * X, 2)) + eps), 0, len(X), len(X)) * X
+    return sp.sparse.spdiags(1. / (np.sqrt(np.sum(X * X, axis=1)) + eps), 0, len(X), len(X)).dot(X)
+    # return sp.sparse.spdiags(1. / (np.sqrt(np.sum(X[:,0]**2) + np.sum(X[:,1]**2)) + eps), 0, X.shape[0], X.shape[0]) * X
+    # return np.sqrt(np.sum(X[:,0]**2) + np.sum(X[:,1]**2))
+    # return sparse.spdiags(1. / (np.sqrt(sum(X * X, 2)) + eps), 0, len(X), len(X)) * X
 
 def tfidf(x_train, x_test):
     # idf = np.divide(x_train.shape[0], (sum(x_train > 0) + eps))
     idf = np.log(np.divide(x_train.shape[0], sum(x_train > 0) + eps))
     idf = idf.reshape(idf.shape[0], 1)
-    myes = idf.conj().T
+    # myes = idf.conj().T
     # print(idf.shape[0])
     # IDF = spdiags(idf', 0, size(idf,2), size(idf,2));
-    IDF = sp.sparse.spdiags(idf.conj().T, 0, idf.shape[0], idf.shape[0])
-    x_train = x_train * IDF
+    IDF = sp.sparse.spdiags(idf.conj().T, 0, idf.shape[0], idf.shape[0]).toarray()
+
+    x_train = x_train.dot(IDF)
+    # print(len(x_train))
+    # x_train = np.linalg.norm(x_train, axis=(0, 1))
     x_train = L2_norm_row(x_train)
 
-    x_test = x_test * IDF
+    x_test = x_test.dot(IDF)
+    # print(len(x_test))
     x_test = L2_norm_row(x_test)
 
     return x_train, x_test
@@ -70,16 +78,16 @@ for x in nips12['apapers']:
 # print(sorted(papers))
 # set(papers)
 indexxx = sorted(set(papers))
-user_item = pd.DataFrame(index=[x for x in range(nd+1)], columns=[x for x in range(na)]).fillna(0)
+user_item = pd.DataFrame(index=[x for x in range(nd+1)], columns=[x for x in range(na)]).fillna(0.0)
 # print(user_item.head())
 for user in user_item.columns:
     # print(user)
     papers = nips12['apapers'][user]
     if isinstance(papers, int):
-        user_item.at[papers, user] = 1
+        user_item.at[papers, user] = 1.0
     else:
         for x in papers:
-            user_item.at[x, user] = 1
+            user_item.at[x, user] = 1.0
 #         paper = nips12['apapers'][user]
 #         # print(paper, user)
 #         user_item[paper][user] = 1
@@ -89,17 +97,22 @@ for user in user_item.columns:
 #             # print(x, user)
 #             user_item[x][user] = 1
 
-# print(user_item)
+mu = user_item.drop([0], axis=0)
 user_item.to_csv("DEMOOOOOO.csv", sep='\t')
-xu = scipy.sparse.csr_matrix(user_item.values.T).toarray()
-T = tmat['T']
+xu = scipy.sparse.csr_matrix(mu.values).toarray()
+T = tmat['T'].astype(np.float32)
 # print(T)
 # print(xdxd)
 # myes = T[:, 0:12]
 # print(myes)
 train_time = np.where(np.sum(T[:, 0:12], axis=1))
+train_time = np.asarray(train_time[0])
+train_time = train_time.reshape(train_time.shape[0], 1)
 # print(len(train_time[0]))
+
 test_time = np.where(np.sum(T[:, 12], axis=1))
+test_time  = np.asarray(test_time[0])
+test_time  = test_time.reshape(test_time.shape[0], 1)
 
 vocab = [x for x in range(nw)] #1d vector
 vocab = np.array(vocab)
@@ -107,12 +120,12 @@ vocab = np.array(vocab)
 # print(test_time)
 
 xu_train = xu[train_time, :]
-counts = nips12['counts'].toarray().astype(np.uint8)
+counts = nips12['counts'].toarray().astype(np.float32)
 
 
 xs_train = counts[:, train_time].conj().T
-xs_train = xs_train[:,0,:]
-
+# xs_train = xs_train[:,0,:]
+xs_train = xs_train[0,:,:]
 print(xs_train)
 # print(len(xs_train[0]), len(xs_train[1]), len(xs_train[2]))
 # print(len(vocab))
@@ -121,32 +134,38 @@ xs_train = xs_train[:, vocab]
 
 xu_test = xu[test_time, :]
 xs_test = counts[:, test_time].conj().T
-xs_test = xs_test[:,0,:]
+xs_test = xs_test[0,:,:]
 xs_test = xs_test[:, vocab]
 
 
-train_authors = (np.sum(xu_train) > 0)
+train_authors = (sum(xu_train) > 0)
 xu_train = xu_train[:, train_authors]
 xu_test = xu_test[:, train_authors]
 
-[xs_train, xs_test] = tfidf(xs_train, xs_test)
+xs_train, xs_test = tfidf(xs_train, xs_test)
 # print(vocab)
 
 k = 500
 alpha = 0.5
 lambdaa = 0.5
 epsilon = 0.001
-maxiter = 500
+maxiter = 10
 verbose = True
 beta = 0.05
 
+# lul = pd.read_csv("../LCE_code/LCE_data/A.csv")
+# aaaa = scipy.sparse.csr_matrix(lul.values).toarray()
 a = construct_A(xs_train, 1, True)
-[w, hs, hu, objhistory] = LCE(xs_train,  L2_norm_row(xu_train), a, k, alpha, beta, lambdaa, epsilon, maxiter, verbose)
+a = a.toarray()
+w, hu, hs, objhistory = LCE(xs_train,  L2_norm_row(xu_train), a, k, alpha, beta, lambdaa, epsilon, maxiter, verbose)
 
-w_test = xs_test / hs
+# w_test = xs_test / hs
+# w_test = np.linalg.solve(xs_test, hs)
+w_test = np.dot(xs_test, np.linalg.pinv(hs))
 w_test[w_test < 0] = 0
-lce_rank = w_test * hu
-lce_res = NDCG(lce_rank, xu_test)
+lce_rank = np.dot(w_test, hu)
+# lce_res = NDCG(lce_rank, xu_test)
+lce_res = sk.metrics.ndcg_score(xu_test, lce_rank)
 print(lce_res)
 
 # for i in user_item.columns:
--- engine/LCE_code/tfidf.py
+++ engine/LCE_code/tfidf.py
@@ -22,17 +22,17 @@ import scipy.sparse
 #
 # end
 
-def tfidf(x_train, x_test):
-    idf = np.divide(x_train.shape[0], (sum(x_train > 0) + np.spacing(1)))
-    # IDF = spdiags(idf', 0, size(idf,2), size(idf,2));
-    IDF = sp.sparse.spdiags(idf, 0, idf.shape[1], idf.shape[1])
-    x_train = x_train * IDF
-    x_train = L2_norm_row(x_train)
+# def tfidf(x_train, x_test):
+#     idf = np.divide(x_train.shape[0], (sum(x_train > 0) + np.spacing(1)))
+#     # IDF = spdiags(idf', 0, size(idf,2), size(idf,2));
+#     IDF = sp.sparse.spdiags(idf, 0, idf.shape[1], idf.shape[1])
+#     x_train = x_train * IDF
+#     x_train = L2_norm_row(x_train)
+#
+#     x_test = x_test * IDF
+#     x_test = L2_norm_row(x_test)
+#
+#     return x_train, x_test
 
-    x_test = x_test * IDF
-    x_test = L2_norm_row(x_test)
-
-    return x_train, x_test
-
-def L2_norm_row(X):
-    return sp.sparse.spdiags(1. / (np.sqrt(sum(X * X, 2)) + eps), 0, len(X), len(X)) * X
+# def L2_norm_row(X):
+#     return sp.sparse.spdiags(1. / (np.sqrt(sum(X * X, 2)) + eps), 0, len(X), len(X)) * X
